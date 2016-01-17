551e2e903d5d5f7b4813e3119d01c8e785f40ba8
Pralie Dutzel <praliedutzel@gmail.com>
2016-01-17 12:08:43 -0700

    Removed emoji support from WP

--- functions.php
+++ functions.php
@@ -21,6 +21,17 @@ add_action( 'init', 'ca_init' );
 
 
 /*-----------------------------------------------------------------------------------*/
+/* Remove emoji support
+/*-----------------------------------------------------------------------------------*/
+
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
+
+
+/*-----------------------------------------------------------------------------------*/
 /* Enable RSS feed support
 /*-----------------------------------------------------------------------------------*/
 
