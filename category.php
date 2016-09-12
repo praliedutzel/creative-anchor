58b00589b4c50a81a548cde41c4a2e1aac1ce78b
Pralie Dutzel <praliedutzel@gmail.com>
2016-09-11 19:06:57 -0700

    Updated the markup for the blog post cards on the category archives

--- category.php
+++ category.php
@@ -12,19 +12,33 @@
                while ( have_posts() ) : the_post();
                   $articleName = get_the_title( $id );
                   $articleIcon = get_category_icons( $id );
+                  $isExternalPost = get_field( 'external_post', $id );
             ?>
-               <li class="grid__fourth"><a href="<?php echo get_the_permalink( $id ); ?>" class="card">
-                  <div class="card__header">
-                     <div class="icon-wrapper">
-                        <svg class="icon">
-                           <use xlink:href="<?php echo $templateDirectory; ?>/img/spritemap.svg#<?php echo $articleIcon; ?>"></use>
-                        </svg>
+               <li class="grid__half flex">
+                  <?php if ( $isExternalPost == 'yes' ) : ?>
+                  <a href="<?php echo get_field( 'external_link', $id ); ?>" target="_blank" class="card">
+                  <?php else : ?>
+                  <a href="<?php echo get_the_permalink( $id ); ?>" class="card">
+                  <?php endif; ?>
+                     <div class="card__teaser">
+                        <div class="icon-wrapper">
+                           <svg class="icon">
+                              <use xlink:href="<?php echo $templateDirectory; ?>/img/spritemap.svg#<?php echo $articleIcon; ?>"></use>
+                           </svg>
+                        </div>
                      </div>
-                  </div>
-                  <div class="card__title">
-                     <h3><?php echo $articleName; ?></h3>
-                  </div>
-               </a></li>
+                     <div class="card__content">
+                        <h3 class="card__title"><?php echo $articleName; ?></h3>
+                        <p class="desktop"><?php echo get_the_excerpt( $id ); ?></p>
+
+                        <?php if ( $isExternalPost == 'yes' ) : ?>
+                           <svg class="icon card__external-link">
+                               <use xlink:href="<?php echo $templateDirectory; ?>/img/spritemap.svg#external-link"></use>
+                           </svg>
+                        <?php endif; ?>
+                     </div>
+                  </a>
+               </li>
             <?php endwhile; ?>
          </ul>
          <?php echo do_shortcode( '[button link="/writings/"]More Articles[/button]' ); ?>
